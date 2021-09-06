40358e4a9276c128aaf70d4c390b56c67b30d90f
陳品諺Willy <62274913+willychen0318@users.noreply.github.com>
2021-09-06 16:23:45 +0800

    Delete final-project-for-1092-java2-willychen0318 directory

--- final-project-for-1092-java2-willychen0318/src/main/java/com/fju/MediumCost.java
+++
@@ -1,9 +0,0 @@
-package com.fju;
-
-public class MediumCost extends Cost {
-    public MediumCost(){
-        price=2;
-        dateline=90;
-        name="medium-term\'s rent =";
-    }
-}
--- final-project-for-1092-java2-willychen0318/src/main/java/com/fju/ShortCost.java
+++
@@ -1,9 +0,0 @@
-package com.fju;
-
-public class ShortCost extends Cost {
-    public ShortCost(){
-        price=1;
-        dateline=45;
-        name="Short-term\'s rent =";
-    }
-}
